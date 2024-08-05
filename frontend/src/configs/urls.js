c8aa527d078624f5c90d98e68da7cda29c727d99
just Nazar <nazar.mraka@gmail.com>
2024-08-05 10:32:15 +0300

    change routes

---
+++ frontend/src/configs/urls.js
@@ -0,0 +1,3 @@
+const baseURL = "https://reenbit-be.vercel.app/api/v1"
+
+export { baseURL };
--- frontend/src/services/axios.service.js
+++ frontend/src/services/axios.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// import { baseURL } from "../configs/urls";
+import { baseURL } from "../configs/urls";
 
-const axiosService = axios.create({ baseURL: "http://localhost:5000" });
+const axiosService = axios.create({ baseURL });
 
 export default axiosService;
